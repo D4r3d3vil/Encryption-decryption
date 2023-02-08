4756b745fa1da7092e80c5816455c860ab393c19
D4r3d3vil <122141636+D4r3d3vil@users.noreply.github.com>
2023-02-08 19:53:48 +0300

    Update encryption.py
    made fingerprint shorter by removing duplicated characters

--- encryption.py
+++ encryption.py
@@ -5,11 +5,12 @@ fingerprint = []
 chars = []
 encrypted_text = []
 for i in range(len(charstr)):
-    chars.append(charstr[i])
-for tr in range(len(charstr)):
+    if not charstr[i] in chars: 
+        chars.append(charstr[i])
+for tr in range(len(chars)):
         rnum = random.random()
         fingerprint.append(rnum)
-        fingerprint.append(charstr[tr])
+        fingerprint.append(chars[tr])
 for ind in range(len(text)):
     encrypted_text.append(fingerprint[fingerprint.index(text[ind]) - 1])
 print("\n"+ "decryption key (dont give this to anyone! (sensitive info may be leaked)):\n\n", str(fingerprint) + "\n\nencrypted text:\n\n" + str(encrypted_text) + "\n")
